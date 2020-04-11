66bcba90dfb4169ed6eaccd1a9f7ca0fde0a1f19
Srinidhi Konda <Srinidhi Konda@LAPTOP-MD74U8GA>
2020-04-11 23:01:56 +0530

    movie info

---
+++ capg-movie-info/src/main/java/com/capg/movieinfo/model/MovieCatelog.java
@@ -0,0 +1,34 @@
+package com.capg.movieinfo.model;
+public class MovieCatelog {
+	private long id;
+	private String movieName;
+	public int port;
+	
+	public MovieCatelog() {
+		 
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public MovieCatelog(Long id, String movieName) {
+		super();
+		this.id = id;
+		this.movieName = movieName;
+	}	
+}

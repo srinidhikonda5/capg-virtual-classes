30059140f796f7c9361465aa075088ca2531e53b
Srinidhi Konda <Srinidhi Konda@LAPTOP-MD74U8GA>
2020-04-27 13:57:13 +0530

    added crud operations

---
+++ fms-passenger-ms/src/main/java/com/capg/fms/passenger/service/IPassengerService.java
@@ -0,0 +1,15 @@
+package com.capg.fms.passenger.service;
+
+import java.util.List;
+
+import com.capg.fms.passenger.model.Passenger;
+
+public interface IPassengerService {
+
+
+	Passenger addPassenger(Passenger passenger);
+	boolean deletePassenger(long passengerUIN);
+	Passenger getPassenger(long passengerUIN);
+	List<Passenger> getAllPasssengers();
+	Passenger updatePassenger(Passenger passenger);
+}
